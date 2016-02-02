49c3ff8481c078d75c8609680d2959c12b09ab1e
techwhiz <saurabh@techwhiz.in>
2016-02-02 17:25:41 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,24 +1,19 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+import scraperwiki           
+import lxml.html
+import re
+import urlparse     
+import requests
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+# We're always asking for json because it's the easiest to deal with
+morph_api_url = "https://api.morph.io/techwhiz/infibooks-list/data.json"
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+# Keep this key secret!
+morph_api_key = "asPO65qEmH9YJO5XepKc"
+
+urls = requests.get(morph_api_url, params={
+  'key': morph_api_key,
+  'query': "select * from 'data' limit 100"
+})
+
+for url in urls:
+  print url
